ae17c6608bccbc172823e4a9b79236e459a0b11c
Yanzhao Wang <yanzhao960808@gmail.com>
2022-01-09 01:38:32 +0100

    update of c langauge icon for nvimtreee

--- lua/plugs/nvimtree_config.lua
+++ lua/plugs/nvimtree_config.lua
@@ -1,3 +1,20 @@
+-- vim.cmd([[highlight IconNameDevicon guifg='<color>' ctermfg='<cterm_color>']])
+require("nvim-web-devicons").setup {
+	override = {
+		C = {
+			icon = "",
+			color = "#13cf2f",
+			-- cterm_color = "",
+			name = "C"
+		}
+	};
+    default = true;
+}
+
+
+
+
+
 vim.cmd([[let g:nvim_tree_special_files = { }]])
 require'nvim-tree'.setup{
     view = {
