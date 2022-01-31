774fa6800f49ea705a9e4a6e324a486a20a3edd3
Yanzhao Wang <yanzhao960808@gmail.com>
2022-01-31 18:14:23 +0100

    bug fixes on clang-format

--- lua/plugs/clangformat_config.lua
+++ lua/plugs/clangformat_config.lua
@@ -1,12 +1,12 @@
-if (vim.fn.executable('clang-format')) then
+if (vim.fn.executable('clang-format') == 1) then
     vim.g['clang_format#command']='clang-format'
-elseif(vim.fn.executable('clang-format-10')) then
+elseif(vim.fn.executable('clang-format-10') == 1) then
     vim.g['clang_format#command']='clang-format-10'
-elseif(vim.fn.executable('clang-format-11')) then
+elseif(vim.fn.executable('clang-format-11') == 1) then
     vim.g['clang_format#command']='clang-format-11'
-elseif(vim.fn.executable('clang-format-12')) then
+elseif(vim.fn.executable('clang-format-12') == 1) then
     vim.g['clang_format#command']='clang-format-12'
-elseif(vim.fn.executable('clang-format-13')) then
+elseif(vim.fn.executable('clang-format-13') == 1) then
     vim.g['clang_format#command']='clang-format-13'
 else
     print('executable clang-format not found')
