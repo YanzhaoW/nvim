d58463c159c939bbce4faea40d096ac16496afcf
Yanzhao Wang <yanzhao960808@gmail.com>
2022-02-24 04:49:56 +0100

    adding scrolling

--- lua/PlugInstall.lua
+++ lua/PlugInstall.lua
@@ -25,6 +25,7 @@ return require('packer').startup(function(use)
     use 'ethanholz/nvim-lastplace'
     use 'rmagatti/auto-session'
     use 'rmagatti/session-lens'
+    use {'karb94/neoscroll.nvim', config = function() require'neoscroll'.setup{} end}
 
     -- lsp plugins
     use 'neovim/nvim-lspconfig'
