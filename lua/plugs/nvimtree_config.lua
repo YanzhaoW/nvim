cd0f6be6243157a2afbe32042277e4eec4dc0de5
YanzhaoW <yanzhao960808@gmail.com>
2022-06-20 22:05:39 +0200

    update nvimtree

--- lua/plugs/nvimtree_config.lua
+++ lua/plugs/nvimtree_config.lua
@@ -25,4 +25,4 @@ require'nvim-tree'.setup{
         ignore_list = {}
     }
 }
-vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
+-- vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
