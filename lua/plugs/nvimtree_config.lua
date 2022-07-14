15f132ccc628d5c4b98cdb87939a8ba33bd70365
YanzhaoW <yanzhao960808@gmail.com>
2022-07-14 17:44:50 +0200

    disable nvim tree from autoopen

--- lua/plugs/nvimtree_config.lua
+++ lua/plugs/nvimtree_config.lua
@@ -19,9 +19,12 @@ require'nvim-tree'.setup{
     view = {
         side = 'right'
     },
+    hijack_directories = {
+        auto_open = false,
+    },
     update_focused_file = {
         enable = true,
         update_cwd = true,
-        ignore_list = {}
+        ignore_list = {},
     }
 }
