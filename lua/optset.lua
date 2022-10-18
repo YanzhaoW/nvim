3d410f26a8771e0de4b28f084825bdbee82e5675
YanzhaoW <yanzhao960808@gmail.com>
2022-10-18 19:15:21 +0200

    update to 0.8.0

--- lua/optset.lua
+++ lua/optset.lua
@@ -17,22 +17,36 @@ vim.opt.termguicolors = true
 vim.opt.pumheight = 15
 vim.opt.clipboard = "unnamedplus"
 vim.opt.ignorecase = true
+vim.opt.spelloptions=noplainbuffer
 
 vim.g.oscyank_silent = true
 
 vim.cmd('filetype plugin indent on')
 -- vim.cmd('hi! MatchParen gui=reverse,bold guibg=reverse')
-vim.cmd([[
-augroup usergroup
-autocmd!
-autocmd FileType text,tex,markdown setlocal spell spelllang=en_us,de_de
-autocmd FileType text,tex,markdown set spellcapcheck=
-au FileType markdown hi! MatchParen gui=bold guifg='#8f96a3'
-au BufNewFile,BufRead tex syntax spell toplevel
-autocmd FileType cpp :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
-autocmd User TelescopePreviewerLoaded setlocal wrap
-augroup END
-]])
+-- vim.cmd([[
+-- augroup usergroup
+-- autocmd!
+-- autocmd FileType text,tex,markdown setlocal spell spelllang=en_us,de_de
+-- autocmd FileType text,tex,markdown set spellcapcheck=
+-- au FileType markdown hi! MatchParen gui=bold guifg='#8f96a3'
+-- au BufNewFile,BufRead tex syntax spell toplevel
+-- autocmd FileType cpp :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+-- autocmd User TelescopePreviewerLoaded setlocal wrap
+-- augroup END
+-- ]])
+
+local my_augroup = vim.api.nvim_create_augroup("mygroup", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "tex", "markdown" }, -- disable spellchecking for these filetypes
+  command = "setlocal spell spelllang=en_us,de_de | set spellcapcheck= | syntax spell toplevel",
+  group = my_augroup,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = { "TelescopePreviewerLoaded" }, -- disable spellchecking for these filetypes
+  command = "setlocal wrap",
+  group = my_augroup,
+})
 
 if (os.getenv("TMUX")~=nil) then
     vim.g.clipboard = {
