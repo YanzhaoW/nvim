045812e986696d3afde337311bf0f89c172a3153
YanzhaoW <yanzhao960808@gmail.com>
2023-01-18 19:46:15 +0100

    clang-format prioritize clang-format-8

--- lua/plugs/clangformat_config.lua
+++ lua/plugs/clangformat_config.lua
@@ -1,5 +1,5 @@
-if (vim.fn.executable('clang-format') == 1) then
-    vim.g['clang_format#command']='clang-format'
+if (vim.fn.executable('clang-format-8') == 1) then
+    vim.g['clang_format#command']='clang-format-8'
 elseif(vim.fn.executable('clang-format-10') == 1) then
     vim.g['clang_format#command']='clang-format-10'
 elseif(vim.fn.executable('clang-format-11') == 1) then
@@ -8,5 +8,7 @@ elseif(vim.fn.executable('clang-format-12') == 1) then
     vim.g['clang_format#command']='clang-format-12'
 elseif(vim.fn.executable('clang-format-13') == 1) then
     vim.g['clang_format#command']='clang-format-13'
+elseif(vim.fn.executable('clang-format') == 1) then
+    vim.g['clang_format#command']='clang-format'
 end
 vim.g['clang_format#auto_format']=0
