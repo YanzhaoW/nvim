9c257a58f2adb4f51ddccb143accee85e39abe36
YanzhaoW <yannzhaow@gmail.com>
2023-05-02 17:06:04 +0200

    fix the clangd settings

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -12,33 +12,32 @@ M.setup = function()
     nvim_lsp.clangd.setup {
         on_attach = on_attach,
         capabilities = capabilities,
-        settings = {
-            cmd = {
-                -- see clangd --help-hidden
-                "clangd",
-                "--background-index",
-                -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
-                -- to add more checks, create .clang-tidy file in the root directory
-                -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
-                "--clang-tidy",
-                "--clang-tidy-checks=*",
-                "--all-scopes-completion",
-                -- "--completion-style=detailed",
-                "--header-insertion-decorators",
-                -- "--header-insertion=iwyu",
-                "--pch-storage=memory",
-                "--completion-style=bundled",
-                "--cross-file-rename",
-                "--header-insertion=iwyu",
-            },
-            init_options = {
-                clangdFileStatus = true, -- Provides information about activity on clangd’s per-file worker thread
-                usePlaceholders = true,
-                completeUnimported = true,
-                semanticHighlighting = true,
-            },
-            single_file_support = true,
-        }
+        cmd = {
+            -- see clangd --help-hidden
+            "clangd",
+            "--background-index",
+            -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
+            -- to add more checks, create .clang-tidy file in the root directory
+            -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
+            "--clang-tidy",
+            "--clang-tidy-checks=*",
+            "--all-scopes-completion",
+            -- "--completion-style=detailed",
+            "--header-insertion-decorators",
+            -- "--header-insertion=iwyu",
+            "--pch-storage=memory",
+            "--completion-style=bundled",
+            "--cross-file-rename",
+            "--header-insertion=iwyu",
+        },
+        init_options = {
+            clangdFileStatus = true, -- Provides information about activity on clangd’s per-file worker thread
+            usePlaceholders = true,
+            completeUnimported = true,
+            semanticHighlighting = true,
+        },
+        root_dir = nvim_lsp.util.root_pattern('.clangd', '.git', 'compile_commands.json'),
+        single_file_support = true,
     }
 end
 
