9fa131c42b1a9887ad739d4d1f53eca68a35600d
Yanzhao Wang <yannzhaow@gmail.com>
2024-11-25 17:00:32 +0100

    add snipets

--- lua/PlugInstall.lua
+++ lua/PlugInstall.lua
@@ -112,7 +112,24 @@ require("lazy").setup({
         "L3MON4D3/LuaSnip",
         -- follow latest release.
         version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        build = "make install_jsregexp"
+        build = "make install_jsregexp",
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+            "benfowler/telescope-luasnip.nvim",
+        },
+        config = function(_, opts)
+            if opts then require("luasnip").config.setup(opts) end
+            vim.tbl_map(
+                function(type) require("luasnip.loaders.from_" .. type).lazy_load() end,
+                { "vscode", "snipmate", "lua" }
+            )
+            -- friendly-snippets - enable standardized comments snippets
+            require("luasnip").filetype_extend("typescript", { "tsdoc" })
+            require("luasnip").filetype_extend("lua", { "luadoc" })
+            require("luasnip").filetype_extend("python", { "pydoc" })
+            require("luasnip").filetype_extend("cpp", { "cppdoc" })
+            require("luasnip").filetype_extend("sh", { "shelldoc" })
+        end
     },
 
     -- "cmp
