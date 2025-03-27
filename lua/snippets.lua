f51c8c47f6ec64d06797276a82701e13eed7d503
YanzhaoW <yannzhaow@gmail.com>
2025-03-27 23:39:39 +0100

    add snippet for refeq

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -14,7 +14,7 @@ lua_snippet.add_snippets("cpp", {
 		fmt(
 			[[
 /** 
- * \brief {}
+ * @brief {}
  *
  * {}
  * @param {} {}
@@ -37,6 +37,7 @@ lua_snippet.add_snippets("cpp", {
 
 lua_snippet.add_snippets("markdown", {
 	snippet("$$", fmt([[@f${}@f$]], { insert(1, "content") })),
+	snippet("refeq", fmt([[@f$(\ref{{eq:{}}})@f$]], { insert(1, "label") })),
 })
 
 lua_snippet.add_snippets("tex", {
