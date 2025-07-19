cf021ede9ae71008e1c2faee1b21526d20fd76b4
YanzhaoW <yannzhaow@gmail.com>
2025-07-19 20:29:08 +0200

    update

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -2,7 +2,7 @@ local capabilities = require("lsp.capability")
 
 require("mason-lspconfig").setup({
     automatic_enable = true,
-    ensure_installed = { "lua_ls", "yamlls", "cmake", "pyright", "clangd", "esbonio", "ltex_plus" },
+    ensure_installed = { "lua_ls", "yamlls", "cmake", "pyright", "clangd", "esbonio", "ltex_plus", "cspell_ls" },
 })
 
 vim.lsp.config("*", {
@@ -10,46 +10,15 @@ vim.lsp.config("*", {
     capabilities = capabilities,
 })
 
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("yamlls")
-vim.lsp.enable("clangd")
-vim.lsp.enable("cmake")
-vim.lsp.enable("python")
-vim.lsp.enable("ltex_plus")
+require("lsp/ltex_plus")
+require("lsp/cspell_ls")
+require("lsp/lua_ls")
+require("lsp/clangd")
+require("lsp/yamlls")
+require("lsp/cmake")
+require("lsp/python")
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = false,
     signs = true,
 })
-
-_G.bufferCopy = function(bufnr)
-    local util = require("lspconfig.util")
-    local winid = vim.fn.winnr()
-    if winid == 1 then
-        if vim.fn.win_getid(2) == 0 then
-            bufnr = util.validate_bufnr(bufnr)
-            vim.fn.execute("rightbelow vertical new")
-        else
-            bufnr = vim.fn.winbufnr(2)
-        end
-    else
-        bufnr = vim.fn.winbufnr(1)
-    end
-    local clangd_client = util.get_active_client_by_name(bufnr, "clangd")
-    local params = { uri = vim.uri_from_bufnr(bufnr) }
-    if clangd_client then
-        clangd_client.request("textDocument/switchSourceHeader", params, function(err, result)
-            if err then
-                error(tostring(err))
-            end
-            if not result then
-                print("Corresponding file on the other window cannot be determined")
-                return
-            end
-            vim.api.nvim_command("edit " .. vim.uri_to_fname(result))
-        end, bufnr)
-    else
-        print("method textDocument/switchSourceHeader is not supported by any servers active on the buffer")
-    end
-end
--- require('lint')
--- lua/plugs/null_ls.lua
+++ lua/plugs/null_ls.lua
@@ -1,17 +1,6 @@
 local null_ls = require "null-ls"
 
 local sources = {
-    -- null_ls.builtins.diagnostics.cppcheck.with({
-    --     --     -- extra_args = { "--project=compile_commands.json" },
-    --     --     -- args = { "--project=compile_commands.json", "--enable=warning,style,performance,portability", "--template=gcc", },
-    --     args = {
-    --         "--language=c++",
-    --         "--enable=warning,style,performance,portability",
-    --         "--template=gcc",
-    --         "--suppress=unusedStructMember",
-    --         "$FILENAME",
-    --     },
-    -- }),
     null_ls.builtins.diagnostics.pylint.with({
         extra_args = { "--disable=C0116,C0115,C0114", "--max-line-length=120" }
     }),
