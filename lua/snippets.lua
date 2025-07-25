05d48b0942bf9c1084da2a5ff11da48cf92c03f5
YanzhaoW <yannzhaow@gmail.com>
2025-07-25 15:30:27 +0200

    add more snippets

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -1,19 +1,19 @@
 local lua_snippet = require("luasnip")
-local extras = require("luasnip.extras")
+-- local extras = require("luasnip.extras")
 local fmt = require("luasnip.extras.fmt").fmt
 
 local snippet = lua_snippet.snippet
-local text = lua_snippet.text_node
+-- local text = lua_snippet.text_node
 local insert = lua_snippet.insert_node
-local rep = extras.rep
+-- local rep = extras.rep
 
 lua_snippet.add_snippets("cpp", {
-	snippet("//!<", fmt([[//!< {}]], { insert(1) })),
-	snippet(
-		"doxfun",
-		fmt(
-			[[
-/** 
+    snippet("//!<", fmt([[//!< {}]], { insert(1) })),
+    snippet(
+        "doxfun",
+        fmt(
+            [[
+/**
  * @brief {}
  *
  * {}
@@ -22,47 +22,60 @@ lua_snippet.add_snippets("cpp", {
  * @see {}
  */
             ]],
-			{
-				insert(1, "brief description"),
-				insert(2, "detailed description"),
-				insert(3, "name"),
-				insert(4, "description"),
-				insert(5, "name"),
-				insert(6, "description"),
-				insert(7, "ref"),
-			}
-		)
-	),
-	snippet(
-		"doxclass",
-		fmt(
-			[[
-/** 
+            {
+                insert(1, "brief description"),
+                insert(2, "detailed description"),
+                insert(3, "name"),
+                insert(4, "description"),
+                insert(5, "name"),
+                insert(6, "description"),
+                insert(7, "ref"),
+            }
+        )
+    ),
+    snippet(
+        "doxclass",
+        fmt(
+            [[
+/**
  * @class {}
  * @brief {}
  *
  * {}
  */
             ]],
-			{
-				insert(1, "class name"),
-				insert(2, "brief description"),
-				insert(3, "description"),
-			}
-		)
-	),
+            {
+                insert(1, "class name"),
+                insert(2, "brief description"),
+                insert(3, "description"),
+            }
+        )
+    ),
+    snippet("cling", fmt(
+        [[
+#ifndef __CLING__
+#endif
+            ]], {}
+    )),
+    snippet("has-cxx-17", fmt(
+        [[
+#ifdef HAS_CXX_17
+#else
+#endif
+            ]], {}
+    )),
 })
 
 lua_snippet.add_snippets("markdown", {
-	snippet("$$", fmt([[@f${}@f$]], { insert(1, "content") })),
-	snippet("refeq", fmt([[@f$\ref{{eq:{}}}@f$]], { insert(1, "label") })),
+    snippet("$$", fmt([[@f${}@f$]], { insert(1, "content") })),
+    snippet("refeq", fmt([[@f$\ref{{eq:{}}}@f$]], { insert(1, "label") })),
 })
 
 lua_snippet.add_snippets("cmake", {
-	snippet(
-		"init",
-		fmt(
-			[[
+    snippet(
+        "init",
+        fmt(
+            [[
 cmake_minimum_required(VERSION 3.28)
 
 project({})
@@ -74,31 +87,31 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 add_executable(main main.cpp)
 target_link_library(main PRIVATE {})
         ]],
-			{
-				insert(1, "project_name"),
-				insert(2, "dependencies"),
-			}
-		)
-	),
+            {
+                insert(1, "project_name"),
+                insert(2, "dependencies"),
+            }
+        )
+    ),
 })
 
 lua_snippet.add_snippets("tex", {
-	snippet(
-		"frame",
-		fmt(
-			[[
+    snippet(
+        "frame",
+        fmt(
+            [[
 \begin{{frame}}[t]{{{}}}
 \end{{frame}}
             ]],
-			{
-				insert(1, "frame title"),
-			}
-		)
-	),
-	snippet(
-		"columns",
-		fmt(
-			[[
+            {
+                insert(1, "frame title"),
+            }
+        )
+    ),
+    snippet(
+        "columns",
+        fmt(
+            [[
 \begin{{columns}}[t]
     \begin{{column}}{{0.45 \textwidth}}
     \end{{column}}
@@ -106,22 +119,22 @@ lua_snippet.add_snippets("tex", {
     \end{{column}}
 \end{{columns}}
             ]],
-			{}
-		)
-	),
-	snippet(
-		"figure",
-		fmt(
-			[[
+            {}
+        )
+    ),
+    snippet(
+        "figure",
+        fmt(
+            [[
 \begin{{figure}}
     \includegraphics[width = \textwidth]{{{}}}
 \end{{figure}}
         ]],
-			{
-				insert(1, "image"),
-			}
-		)
-	),
+            {
+                insert(1, "image"),
+            }
+        )
+    ),
 })
 
 require("keymapping").snippet()
