3a3272689c1c88fffadb3f57bc0b6f9658c6a57c
Yanzhao Wang <yannzhaow@gmail.com>
2025-12-15 20:42:41 +0100

    Update yaml

--- lua/lsp/yamlls.lua
+++ lua/lsp/yamlls.lua
@@ -1,17 +1,12 @@
 vim.lsp.config.yamlls = {
-    filetypes = { "yaml" },
-    cmd = { "yaml-language-server", "--stdio" },
+    filetypes = { "yaml", },
+    cmd = { "yaml-language-server", "--stdio", },
     settings = {
         yaml = {
-            format = {
-                enable = true,
-                singleQuote = true,
-                printWidth = 75,
-            },
-            schemas = {
-                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-            },
+            format = { enable = true, singleQuote = true, printWidth = 75, },
+            -- schemas = { ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*", },
+            schemasStore = { enable = true, },
         },
     },
 }
-vim.lsp.enable("yamlls")
+vim.lsp.enable "yamlls"
