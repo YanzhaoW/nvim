6aad26544e020ef6319ab6ebd305d051b612245b
Yanzhao Wang <yannzhaow@gmail.com>
2026-02-02 13:30:42 +0100

    Add python debugger

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -7,7 +7,7 @@ vim.lsp.config.clangd = {
         "--clang-tidy",
         "--all-scopes-completion",
         "--header-insertion-decorators",
-        "--experimental-modules-support", -- this breaks the single file
+        -- "--experimental-modules-support", -- this breaks the single file
         -- "--debug-modules-builder",
         -- "--log=verbose",
         "--pch-storage=memory",
--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -51,6 +51,65 @@ dap.configurations.cpp = {
 }
 
 
+dap.adapters.python = function(cb, config)
+    if config.request == 'attach' then
+        ---@diagnostic disable-next-line: undefined-field
+        local port = (config.connect or config).port
+        ---@diagnostic disable-next-line: undefined-field
+        local host = (config.connect or config).host or '127.0.0.1'
+        cb {
+            type = 'server',
+            port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+            host = host,
+            options = { source_filetype = 'python', },
+        }
+    else
+        cb {
+            type = 'executable',
+            command = 'debugpy-adapter',
+            options = { source_filetype = 'python', },
+        }
+    end
+end
+
+
+dap.configurations.python = {
+    {
+        -- The first three options are required by nvim-dap
+        type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+        request = 'launch',
+        name = "Launch file",
+
+        -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+        -- program = "${file}", -- This configuration will launch the current file if used.
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd(), 'file')
+        end,
+        pythonPath = function()
+            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+            local virtual_env = os.getenv "VIRTUAL_ENV"
+            local cwd = vim.fn.getcwd()
+            if virtual_env ~= nil then
+                local command = os.getenv "VIRTUAL_ENV" .. "/bin/python"
+                if vim.fn.executable(cwd .. command) == 1 then
+                    return cwd .. command
+                elseif vim.fn.executable(cwd .. command) == 1 then
+                    return cwd .. '/.venv/bin/python'
+                else
+                    return 'python3'
+                end
+            else
+                return 'python3'
+            end
+        end,
+        console = "integratedTerminal",
+    },
+}
+
+
 dapui.setup {
     layouts = {
         {
