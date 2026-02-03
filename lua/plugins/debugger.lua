a2844ddf139345995e3bee1f184c32688d952d61
Yanzhao Wang <yannzhaow@gmail.com>
2026-02-03 16:24:48 +0100

    Update keymapping

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,6 +1,6 @@
 local dap = require "dap"
 
-dapui = require "dapui"
+local dapui = require "dapui"
 
 dapui.setup()
 
@@ -13,43 +13,21 @@ dap.adapters.gdb = {
     args = { "--interpreter=dap", },
 }
 
+dap.adapters.lldb = { type = 'executable', command = 'lldb-dap', name = 'lldb', }
+
 dap.configurations.cpp = {
     {
-        name = "Launch",
-        type = "gdb",
-        request = "launch",
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
         program = function()
             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/build/', 'file')
         end,
-        args = {}, -- provide arguments if needed
-        cwd = "${workspaceFolder}",
-        stopAtBeginningOfMainSubprogram = false,
-    },
-    {
-        name = "Select and attach to process",
-        type = "gdb",
-        request = "attach",
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        pid = function()
-            local name = vim.fn.input 'Executable name (filter): '
-            return require "dap.utils".pick_process { filter = name, }
-        end,
         cwd = '${workspaceFolder}',
-    },
-    {
-        name = 'Attach to gdbserver :1234',
-        type = 'gdb',
-        request = 'attach',
-        target = 'localhost:1234',
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
     },
 }
-
 
 dap.adapters.python = function(cb, config)
     if config.request == 'attach' then
